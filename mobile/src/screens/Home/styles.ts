9b5fa4fea3e60ba1bdba671517c40d161c44fdc8
Filipe Fernandes <filipeferibeiro@gmail.com>
2021-10-21 22:37:36 -0300

    Project server, web and mobile

---
+++ mobile/src/screens/Home/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { COLORS } from '../../theme';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.BLACK_SECONDARY,
+        paddingTop: getStatusBarHeight() + 17
+    }
+});
+
+export { styles }
---
+++ server/src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+    sub: string
+}
+
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+    const authToken = request.headers.authorization;
+
+    if(!authToken) {
+        return response.status(401).json({
+            errorCode: "token.invalid"
+        });
+    }
+
+    // Bearer r5234rqweraertwe453w4rawrsf43tw
+    // [0] Bearer
+    // [1] r5234rqweraertwe453w4rawrsf43tw
+
+    const [, token ] = authToken.split(" ");
+
+    try {
+        const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
+
+        request.user_id = sub;
+
+        return next();
+    } catch(err) {
+        return response.status(401).json({
+            errorCode: "token.expired"
+        });
+    }
+}
